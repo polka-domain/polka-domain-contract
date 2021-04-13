ad2088046107ecbd25de00b7871557bf463af6c3
Alkaid <alkaidwe@gmail.com>
2021-04-13 23:56:56 +0800

    script for deploy airdrop contract

---
+++ scripts/deploy-airdrop.js
@@ -0,0 +1,18 @@
+const {ethers, upgrades} = require("hardhat");
+
+async function main() {
+    const signer = '0x36f302d5fd122ac9f1dd6d5d2600e28f059b45d9';
+    const nameAddress = '0xe1A4c5BBb704a92599FEdB191f451E0d3a1ed842';
+    const amount = ethers.utils.parseEther('20');
+    const Airdrop = await ethers.getContractFactory("Airdrop");
+    const airdrop = await upgrades.deployProxy(Airdrop, [signer, nameAddress, amount]);
+    await airdrop.deployed();
+    console.log("Airdrop deployed to:", airdrop.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
