4cd5e25d99338cd289d0289914ec5fa0c89520e6
Alkaid <alkaidwe@gmail.com>
2021-04-15 14:14:07 +0800

    airdrop should have start and end time

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -5,23 +5,31 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 
 contract Airdrop is OwnableUpgradeable {
     using ECDSAUpgradeable for bytes32;
+    using SafeMathUpgradeable for uint;
 
     IERC20Upgradeable public nameToken;
     uint public amount;
     address public signer;
     mapping(address => bool) public claimed;
+    uint public startAt;
+    uint public duration;
 
-    function initialize(address signer_, address nameAddress_, uint amount_) public initializer {
+    function initialize(address signer_, address nameAddress_, uint amount_, uint startAt_) public initializer {
         super.__Ownable_init();
         signer = signer_;
         nameToken = IERC20Upgradeable(nameAddress_);
         amount = amount_;
+        startAt = startAt_;
+        duration = 48 hours;
     }
 
     function claim(bytes memory signature) external checkClaim checkSign(signature) {
+        require(startAt <= block.timestamp, "CLAIM NOT OPEN");
+        require(block.timestamp < startAt.add(duration), "CLAIM CLOSED");
         claimed[msg.sender] = true;
         nameToken.transfer(msg.sender, amount);
     }
